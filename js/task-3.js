e877f899c840d081aa80c8abb203746426290075
Yasin Özmen <yasinozmeen3@gmail.com>
2025-05-14 19:36:01 +0300

    .

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value) {
+  let a = [];
+  for (const i of numbers) {
+    if (value < i) {
+      a.push(i);
+    }
+  }
+  return a.sort();
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
